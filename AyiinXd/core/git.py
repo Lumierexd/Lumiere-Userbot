e7aa68c38d5042dd2fe1f43ee6f9980bbf1ad956
Lumierexd <122657125+Lumierexd@users.noreply.github.com>
2023-01-31 19:40:23 +0700

    Update git.py

--- AyiinXd/core/git.py
+++ AyiinXd/core/git.py
@@ -61,4 +61,4 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGS.info("Fetched Updates from Ayiin-Userbot")
+        LOGS.info("Fetched Updates from Lumiere-Userbot")
